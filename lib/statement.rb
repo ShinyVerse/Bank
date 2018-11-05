d6c58c68c61e88d92f376551c0d9081c172ac5fb
Laura Jackson <playeronereadystart@gmail.com>
2018-11-05 17:07:09 +0000

    Change statements to be in descending order

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,20 +6,20 @@ class Statement
   end
 
   def deposit(amount, current_balance, transaction_date)
-    @history.push(
+    @history.unshift({
       date: transaction_date,
       credit: amount,
       debit: '',
-      balance: current_balance
+      balance: current_balance}
     )
   end
 
   def withdraw(amount, current_balance, transaction_date)
-    @history.push(
+    @history.unshift({
       date: transaction_date,
       credit: '',
       debit: amount,
-      balance: current_balance
+      balance: current_balance}
     )
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,6 +7,23 @@ describe Statement do
     it 'starts with a blank history' do
       expect(statement.history.length).to eq 0
     end
+
+    describe 'Statements appear in descending order' do
+      before(:each) do
+        statement.deposit(30, 1030, '12/04/12')
+        statement.deposit(30, 1060, '14/04/12')
+      end
+      it 'by adding the newest statement into first position' do
+        expect(statement.history).to eq([{ date: '14/04/12',
+          credit: 30,
+          debit: '',
+          balance: 1060 },
+          { date: '12/04/12',
+            credit: 30,
+            debit: '',
+            balance: 1030 }])
+      end
+    end
   end
 
   describe '#deposit' do
