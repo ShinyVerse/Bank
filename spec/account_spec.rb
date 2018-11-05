37f8663ee4060544da0802451a20aa2c39626875
Laura Jackson <playeronereadystart@gmail.com>
2018-11-05 11:01:03 +0000

    Initial test setup

---
+++ spec/account_spec.rb
@@ -0,0 +1,13 @@
+require 'account'
+
+describe Account do
+  let(:account) { described_class.new }
+
+  describe 'An account instance' do
+    it 'has a history and balance' do
+      expect(account.balance).to eq 0
+      expect(account.history.length).to eq 0
+    end
+  end
+
+end
